6696c0d43c63e5b8cc25a2c6830f353e997ed876
aimaev <albert.voiceinyou@gmail.com>
2018-03-28 02:09:09 +0500

    seems to be done

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 'use strict';
-
+/*  eslint-disable no-lonely-if  */
 let input = document.getElementsByClassName('search-form-tablet__input')[0];
+let navigation = document.getElementsByClassName('navigation')[0];
+let buter = document.getElementsByClassName('header__menu-button')[0];
 
-document.getElementsByClassName('search-form-tablet__button')[0].addEventListener('click', function (e) {
-    input.style.visibility = 'visible';
-    input.focus();
-});
+document.getElementsByClassName('search-form-tablet__button')[0]
+    .addEventListener('click', function () {
+        input.style.visibility = 'visible';
+        input.focus();
+    });
 
-input.addEventListener('focusout', function (e) {
+input.addEventListener('focusout', function () {
     input.style.visibility = 'hidden';
 });
+
+buter.addEventListener('click', activateNavigation);
+
+document.getElementsByClassName('navigation__close-button')[0]
+    .addEventListener('click', function () {
+        navigation.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+
+function activateNavigation() {
+    navigation.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+}
