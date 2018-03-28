ce2bed1f159a6fc095935e3966c8bcc303637454
levshin <M.Levshin@hostco.ru>
2018-03-29 00:24:55 +0500

    lint

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,21 @@
 const getById = id => document.getElementById(id);
 
-const log = console.info;
-
 window.onload = () => {
     const checkbox = getById('search__show-button');
-    const input = getById('search__input')
+    const input = getById('search__input');
 
-    if (window.innerWidth <= 850)
+    if (window.innerWidth <= 850) {
         input.setAttribute('disabled', true);
-
-    checkbox.onchange = () => {
-        if (checkbox.checked)
-            input.setAttribute('disabled', true);
-        else
-            input.removeAttribute('disabled');
-        input.focus();
     }
 
+    checkbox.onchange = () => {
+        if (checkbox.checked) {
+            input.setAttribute('disabled', true);
+        } else {
+            input.removeAttribute('disabled');
+        }
+        input.focus();
+    };
+
     input.onblur = () => checkbox.click();
-}
+};
