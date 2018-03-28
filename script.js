4727d4905a24dc1ee63e74c74e99298fe02869f3
aimaev <albert.voiceinyou@gmail.com>
2018-03-28 19:50:39 +0500

    swipe added

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
 'use strict';
-/*  eslint-disable no-lonely-if  */
-let input = document.getElementsByClassName('search-form-tablet__input')[0];
+
+let tabletInput = document.getElementsByClassName('search-form-tablet__input')[0];
+let mobileInput = document.getElementsByClassName('search-form-mobile__input')[0];
 let navigation = document.getElementsByClassName('navigation')[0];
 let buter = document.getElementsByClassName('header__menu-button')[0];
-
-document.getElementsByClassName('search-form-tablet__button')[0]
-    .addEventListener('click', function () {
-        input.style.visibility = 'visible';
-        input.focus();
-    });
-
-input.addEventListener('focusout', function () {
-    input.style.visibility = 'hidden';
-});
-
-buter.addEventListener('click', activateNavigation);
-
-document.getElementsByClassName('navigation__close-button')[0]
-    .addEventListener('click', function () {
-        navigation.style.display = 'none';
-        document.body.style.overflow = '';
-    });
+let tabletSearchButton = document.getElementsByClassName('search-form-tablet__button')[0];
+let navigationCloseButton = document.getElementsByClassName('navigation__close-button')[0];
+let body = document.body;
 
 function activateNavigation() {
     navigation.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
+    body.style.overflow = 'hidden';
+    mobileInput.setAttribute('disabled', 'disabled');
 }
+
+function disActivateNavigation() {
+    navigation.style.display = 'none';
+    body.style.overflow = '';
+    mobileInput.removeAttribute('disabled');
+}
+
+function activateInput() {
+    tabletInput.style.visibility = 'visible';
+    tabletInput.focus();
+}
+
+function disActivateInput() {
+    tabletInput.style.visibility = 'hidden';
+}
+
+let startX;
+let startY;
+let distX;
+let distY;
+let minDistX = 100;
+let startTime;
+let timeElapsed;
+let maxTime = 250;
+
+body.addEventListener('touchstart', function (e) {
+    startX = e.changedTouches[0].pageX;
+    startY = e.changedTouches[0].pageY;
+    startTime = Date.now();
+}, false);
+
+body.addEventListener('touchend', function (e) {
+    distX = e.changedTouches[0].pageX - startX;
+    distY = e.changedTouches[0].pageY - startY;
+    timeElapsed = Date.now() - startTime;
+    if (timeElapsed <= maxTime &&
+        Math.abs(distX) >= minDistX &&
+        Math.abs(distX) >= Math.abs(distY) &&
+        distX < 0) {
+        activateNavigation();
+    }
+}, false);
+
+
+tabletSearchButton.addEventListener('click', activateInput);
+tabletInput.addEventListener('focusout', disActivateInput);
+
+buter.addEventListener('click', activateNavigation);
+navigationCloseButton.addEventListener('click', disActivateNavigation);
