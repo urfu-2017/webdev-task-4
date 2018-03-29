9a4839570b8033c4d22851f8192781c22abc0073
lms <lms@skbkontur.ru>
2018-03-29 15:35:15 +0500

    make task

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    //let button = document.querySelector('.market-info__serach-button-img')
+    //button.onclick = () => {
+        //document.querySelector('.market-info__search-field').focus();
+    //}
+}
+
+function getFocus() {
+    document.querySelector('.market-info__search-field').focus();
+}
