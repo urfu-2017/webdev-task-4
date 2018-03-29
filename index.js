9f32904c163d733d273a60df6a8b5ab377862198
Alexey <a.dubrovin@skbkontur.ru>
2018-03-29 14:09:57 +0500

    tablet second

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ window.onload = () => {
     const burgerMenu = document.querySelector('.header__burger');
     const navigationClose = document.querySelector('.navigation__close');
     const navigation = document.querySelector('.navigation');
+    const search = document.querySelector('.search');
+    const searchInput = document.querySelector('.search__input');
+    const searchIcon = document.querySelector('.header__search-icon');
 
     burgerMenu.onclick = () => {
         navigation.style.display = 'flex';
@@ -9,5 +12,15 @@ window.onload = () => {
 
     navigationClose.onclick = () => {
         navigation.style.display = 'none';
+    };
+
+    searchIcon.onclick = () => {
+        search.style.display = 'flex';
+        searchIcon.classList.remove('fa-search');
+    };
+
+    searchInput.onblur = () => {
+        search.style.display = 'none';
+        searchIcon.classList.add('fa-search');
     };
 };
