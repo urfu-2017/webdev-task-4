f1870346ee2ecfdd7b67e871756ae29b8ae25c21
Gusarov Vadim <gusarovvadim1996@gmail.com>
2018-03-29 16:41:22 +0500

    remove hammer

--- model.js
+++ model.js
@@ -15,13 +15,31 @@ function hiddenSearch() {
         el.style.display = 'none';
     }
 }
-
-let page = document.querySelector(".mainPart");
-let hammer = new Hammer(page);
-hammer.on('swipeleft', () => {
-    document.getElementById('sandwich').click();
-})
-hammer.on('swiperight', () => {
-    document.getElementById('sandwich').click();
-})
 /* eslint-enable */
+
+let startX;
+let startY;
+let startTime;
+const minDistanceX = 100;
+const maxTime = 250;
+
+let page = document.querySelector('.mainPart');
+
+page.addEventListener('touchstart', function (event) {
+    startX = event.changedTouches[0].pageX;
+    startY = event.changedTouches[0].pageY;
+    startTime = Date.now();
+}, false);
+
+page.addEventListener('touchend', function (event) {
+    let distanceX = event.changedTouches[0].pageX - startX;
+    let distanceY = event.changedTouches[0].pageY - startY;
+    let timeElapsed = Date.now() - startTime;
+    let sandwich = document.getElementById('sandwich');
+    if (timeElapsed <= maxTime &&
+        Math.abs(distanceX) >= minDistanceX &&
+        Math.abs(distanceX) >= Math.abs(distanceY) &&
+        distanceX < 0 && !sandwich.checked) {
+        sandwich.click();
+    }
+}, false);
