6bf1ee3bb2130f8c3787bf1cfa5b23364dfa2e0b
ftlka <smart.Julia@yandex.ru>
2018-03-29 14:17:44 +0500

    postcss configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); /* eslint-disable-line */
 module.exports = { /* eslint-disable-line */
     entry: './src/toggleNav.js',
     output: {
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
     }
 };
