e441adfe97646ad37408a026fde389d093afb96a
ftlka <smart.Julia@yandex.ru>
2018-03-29 16:39:52 +0500

    Preventing scrolling on phones while nav menu is open. Item rating has a triangle on its right side

--- src/toggleNav.js
+++ src/toggleNav.js
@@ -1,14 +1,23 @@
 'use strict';
 const nav = document.querySelector('.nav');
+
+const fixed = document.querySelector('.body');
+
 const showNav = () => {
     nav.className = 'nav white-block center visible';
+    fixed.classList = 'body noScroll';
 };
 
 const hideNav = () => {
     nav.className = 'nav white-block center hidden';
+    fixed.classList = 'body';
 };
+
 
 const sandwichButton = document.querySelector('.phone-sandwich');
 const closeButton = document.querySelector('.nav__close');
 sandwichButton.addEventListener('click', showNav);
+sandwichButton.addEventListener('touchend', showNav);
 closeButton.addEventListener('click', hideNav);
+closeButton.addEventListener('touchend', hideNav);
+
