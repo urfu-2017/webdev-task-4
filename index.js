17374c6c794e08f7b1141e5536188a75c55b476b
lms <lms@skbkontur.ru>
2018-03-29 15:47:39 +0500

    linter

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
-    //let button = document.querySelector('.market-info__serach-button-img')
-    //button.onclick = () => {
-        //document.querySelector('.market-info__search-field').focus();
-    //}
-}
-
-function getFocus() {
-    document.querySelector('.market-info__search-field').focus();
+    let button = document.querySelector('.market-info__serach-button-img');
+    button.onclick = () => {
+        document.querySelector('.market-info__search-field').focus();
+    };
 }
