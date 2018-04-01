e312339ad483791719af72b5d93aae4d5d958180
levshin <M.Levshin@hostco.ru>
2018-04-01 17:51:45 +0500

    M O B U L O C H K A

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,38 @@
 const getById = id => document.getElementById(id);
 
-window.onload = () => {
-    const checkbox = getById('search__show-button');
-    const input = getById('search__input');
+const showMenu = () => {
+    document.body.className = '';
+    getById('menu').className = 'menu';
+};
 
-    if (window.innerWidth <= 850) {
-        input.setAttribute('disabled', true);
+const closeMenu = () => {
+    document.body.className = 'body__mobile-menu-hide';
+    getById('menu').className = 'menu menu__mobile-menu-hide';
+};
+
+let touchX = 0;
+let touchY = 0;
+
+const onSwipe = (x, y) => {
+    const dy = Math.abs(touchY - y);
+    const dx = Math.abs(touchX - x);
+    const isNotRandomness = dy * 2 < dx && dx > 100;
+    if (x < touchX && isNotRandomness) {
+        showMenu();
     }
+
+    if (x > touchX && isNotRandomness) {
+        closeMenu();
+    }
+};
+
+window.onload = () => {
+    const checkbox = getById('search-tablet__show');
+    const input = getById('search-tablet__input');
+    const menuShowButton = getById('header__menu-button');
+    const menuCloseButton = getById('menu__mobile-close');
+
+    input.setAttribute('disabled', true);
 
     checkbox.onchange = () => {
         if (checkbox.checked) {
@@ -17,9 +43,18 @@ window.onload = () => {
         input.focus();
     };
 
-    input.onblur = () => {
-        if (window.innerWidth <= 850) {
-            checkbox.click();
-        }
+    input.onblur = () => checkbox.click();
+
+    menuShowButton.onclick = showMenu;
+
+    menuCloseButton.onclick = closeMenu;
+
+    document.body.ontouchstart = e => {
+        touchX = e.changedTouches[0].screenX;
+        touchY = e.changedTouches[0].screenY;
+    };
+
+    document.body.ontouchend = e => {
+        onSwipe(e.changedTouches[0].screenX, e.changedTouches[0].screenY);
     };
 };
