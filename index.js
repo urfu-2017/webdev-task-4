034e883e9e49137ba02e516ad49fd248e053a240
Maxim Akulin <maxim.akulin@gmail.com>
2018-04-04 02:11:24 +0500

    mobile device

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const searchButton = document.querySelector('.heading__search-btn');
 searchButton.addEventListener('click', function () {
     document.querySelector('.heading__search-line').focus();
 });
+const navigationCloseButton = document.querySelector('.navigaton__close-button');
+
+const sandwichButton = document.querySelector('.sandwich__button');
+sandwichButton.addEventListener('click', function () {
+    document.querySelector('.navigation-wrapper').style.display = 'block';
+    navigationCloseButton.style.display = 'block';
+});
+
+
+navigationCloseButton.addEventListener('click', function () {
+    document.querySelector('.navigation-wrapper').style.display = 'none';
+    navigationCloseButton.style.display = 'none';
+});
