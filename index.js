9bd78f5831470a42e0bbe9bf19a94bde88d7cd55
Maxim Akulin <maxim.akulin@gmail.com>
2018-04-03 23:44:05 +0500

    tablet

---
+++ index.js
@@ -0,0 +1,4 @@
+const searchButton = document.querySelector('.heading__search-btn');
+searchButton.addEventListener('click', function () {
+    document.querySelector('.heading__search-line').focus();
+});
