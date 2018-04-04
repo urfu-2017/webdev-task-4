ab157083115e5f15f8464347be051ff4800e48dd
Dmitry <dinvk@mail.ru>
2018-04-04 15:13:28 +0500

    remove hammer usage

--- scripts/swipe.js
+++ scripts/swipe.js
@@ -1,16 +1,21 @@
 const page = document.querySelector('.page');
 const ticker = document.getElementById('nav-ticker');
-/* eslint-disable no-undef */
-const hammer = new Hammer(page);
-hammer.on('swipeleft', () => {
-    if (!ticker.checked) {
+const changePageOverflowStyle = () => {
+    page.classList.toggle('page_overflow_hidden');
+};
+
+let startX;
+page.addEventListener('touchstart', e => {
+    const [touch] = e.changedTouches;
+    startX = touch.pageX;
+});
+
+page.addEventListener('touchend', e => {
+    const [touch] = e.changedTouches;
+    const distanceX = touch.pageX - startX;
+    if (Math.abs(distanceX) > page.clientWidth / 3) {
         ticker.click();
-        page.style.overflow = 'hidden';
     }
 });
 
-ticker.onclick = () => {
-    if (!ticker.checked) {
-        page.style.overflow = 'scroll';
-    }
-};
+ticker.onclick = changePageOverflowStyle;
