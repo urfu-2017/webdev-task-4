26edb16c4e0772e5f16efac33cbbc46699fdfd7b
Behemothhh <elakov111@gmail.com>
2018-04-05 12:16:01 +0500

    remove hammer.js

--- index.js
+++ index.js
@@ -11,18 +11,42 @@ class Menu {
     }
 
     __addListeners() {
-        Hammer(this.body).on('swipeleft', () => this.__show());
-        Hammer(this.body).on('swiperight', () => this.__close());
+        this.body.addEventListener('touchstart', e => {
+            this.touch = e.touches[0];
+            this.startX = this.touch.clientX;
+            this.startY = this.touch.clientY;
+        });
+        this.body.addEventListener('touchend', e => {
+            this.touchEnd = e.changedTouches[0];
+            this.endX = this.touchEnd.clientX;
+            this.endY = this.touchEnd.clientY;
+            if (this.startX - this.endX > 100 && Math.abs(this.endY - this.startY) < 40) {
+                this.__show();
+            }
+            if (this.startX - this.endX < -100 && Math.abs(this.endY - this.startY) < 40) {
+                this.__close();
+            }
+        });
+
         this.input.addEventListener('change', () => this.__toggleScroll());
+    }
+
+    __clearCoordinates() {
+        this.startX = 0;
+        this.startY = 0;
+        this.endX = 0;
+        this.endY = 0;
     }
 
     __show() {
         this.input.checked = true;
+        this.__clearCoordinates();
         this.__toggleScroll();
     }
 
     __close() {
         this.input.checked = false;
+        this.__clearCoordinates();
         this.__toggleScroll();
     }
 
@@ -42,7 +66,7 @@ class Menu {
     __preventDefault(e) {
         e.preventDefault();
     }
-};
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const menu = new Menu();
