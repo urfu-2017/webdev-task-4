0c046136ce7d4176546d483b74f71f101c6c27b5
spt30 <seregatoschilin@gmail.com>
2018-04-19 14:21:28 +0500

    правки

--- public/search-click-handler.js
+++ public/search-click-handler.js
@@ -4,48 +4,46 @@ const input = searchField.getElementsByClassName('search__input')[0];
 
 loope.addEventListener('click', () => {
     if (loope.checked === true) {
-        searchField.style.display = 'flex';
+        searchField.classList.add('search_visible');
+        searchField.classList.remove('search_hidden');
         input.focus();
         input.onblur = () => {
-            searchField.style.display = 'none';
+            searchField.classList.remove('search_visible');
+            searchField.classList.add('search_hidden');
             loope.checked = false;
         };
     } else {
-        searchField.style.display = 'none';
+        searchField.classList.remove('search_visible');
+        searchField.classList.add('search_hidden');
     }
 });
 
 const sandwich = document.getElementsByClassName('menu_show-button')[0];
 const body = document.getElementsByClassName('body')[0];
 const menu = document.getElementsByClassName('menu')[0];
-const height = Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight
-);
 const currentWidth = document.body.clientWidth;
 
 function showMenu(e) {
     e.stopPropagation();
-    menu.style.height = `${height}px`;
-    menu.style.width = '50%';
     menu.style.left = '50%';
     body.addEventListener('click', () => {
-        menu.style.display = 'none';
-        body.style['overflow-y'] = 'auto';
+        menu.style.left = '100%';
+        menu.classList.remove('menu_shade_end');
+        menu.classList.remove('menu_display_block');
+        body.classList.remove('body_scroll-lock');
     });
     menu.addEventListener('click', (eMenu) => {
         eMenu.stopPropagation();
     });
 }
 
-sandwich.addEventListener('click', (e) => {
+sandwich.addEventListener('click', e => {
     if (getComputedStyle(menu).display === 'none') {
-        menu.style.display = 'block';
-        body.style['overflow-y'] = 'hidden';
+        menu.classList.add('menu_display_block');
+        body.classList.add('body_scroll-lock');
     } else {
-        menu.style.display = 'none';
-        body.style['overflow-y'] = 'auto';
+        menu.classList.remove('menu_display_block');
+        body.classList.remove('body_scroll-lock');
     }
     showMenu(e);
 });
@@ -61,13 +59,15 @@ const touchMoveHandler = e => {
 
 const touchEndHandler = e => {
     if (e.changedTouches[0].pageX < currentWidth / 2) {
-        menu.style.display = 'block';
-        body.style['overflow-y'] = 'hidden';
+        menu.classList.add('menu_display_block');
+        body.classList.add('body_scroll-lock');
+        showMenu(e);
     } else {
-        menu.style.display = 'none';
-        body.style['overflow-y'] = 'auto';
+        menu.classList.remove('menu_display_block');
+        body.classList.remove('body_scroll-lock');
+        menu.style.left = '100%';
     }
-    showMenu(e);
+
     document.removeEventListener('touchmove', touchMoveHandler);
     document.removeEventListener('touchend', touchEndHandler);
 };
@@ -77,12 +77,7 @@ if (currentWidth <= 576) {
         e.stopPropagation();
         const tapPlaceX = e.changedTouches[0].pageX;
         if ((tapPlaceX / currentWidth) > 0.95) {
-            menu.style.height = `${height}px`;
-            menu.style['overflow-x'] = 'hidden';
-            menu.style.display = 'block';
-            // menu.style.width = '0%';
-            menu.style.left = '100%';
-            menu.style.border = 'none';
+            menu.classList.add('menu_display_block');
             document.addEventListener('touchmove', touchMoveHandler);
             document.addEventListener('touchend', touchEndHandler);
         }
